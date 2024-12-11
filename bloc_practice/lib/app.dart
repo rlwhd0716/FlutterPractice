31ec7236ddf5fc4ec2014e58755d3b7679cb4dc3
이기종 <rlwhd0716@gmail.com>
2024-12-11 14:59:04 +0900

    [Bloc Login] 1. domain packages 2. repository pattern 3. splash-login-auth-main 4. route function

--- bloc_practice/lib/app.dart
+++ bloc_practice/lib/app.dart
@@ -1,5 +1,11 @@
-import 'package:bloc_practice/infinite_list/posts/posts.dart';
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc_practice/flutter_login//splash/splash.dart';
+import 'package:bloc_practice/flutter_login/authentication/bloc/authentication_bloc.dart';
+import 'package:bloc_practice/flutter_login/home/home.dart';
+import 'package:bloc_practice/flutter_login/login/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
 
 // class CounterApp extends MaterialApp {
 //   const CounterApp({super.key}) : super(home: const CounterPage());
@@ -22,6 +28,88 @@ import 'package:flutter/material.dart';
 //   }
 // }
 
-class App extends MaterialApp {
-  const App({super.key}) : super(home: const PostsPage());
+// class App extends MaterialApp {
+//   const App({super.key}) : super(home: const PostsPage());
+// }
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late final AuthenticationRepository _authenticationRepository;
+  late final UserRepository _userRepository;
+
+  @override
+  void initState() {
+    super.initState();
+    _authenticationRepository = AuthenticationRepository();
+    _userRepository = UserRepository();
+  }
+
+  @override
+  void dispose() {
+    _authenticationRepository.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: BlocProvider(
+        lazy: false,
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: _authenticationRepository,
+          userRepository: _userRepository,
+        )..add(AuthenticationSubscriptionRequested()),
+        child: const AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  const AppView({super.key});
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  NavigatorState get _navigator => _navigatorKey.currentState!;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: _navigatorKey,
+      builder: (context, child) {
+        return BlocListener<AuthenticationBloc, AuthenticationState>(
+          listener: (context, state) {
+            switch (state.status) {
+              case AuthenticationStatus.authenticated:
+                _navigator.pushAndRemoveUntil<void>(
+                  HomePage.route(),
+                  (route) => false,
+                );
+              case AuthenticationStatus.unauthenticated:
+                _navigator.pushAndRemoveUntil(
+                  LoginPage.route(),
+                  (route) => false,
+                );
+              case AuthenticationStatus.unknown:
+                break;
+            }
+          },
+          child: child,
+        );
+      },
+      onGenerateRoute: (_) => SplashPage.route(),
+    );
+  }
 }
--- bloc_practice/lib/main.dart
+++ bloc_practice/lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:bloc/bloc.dart';
 import 'package:bloc_practice/app.dart';
-import 'package:bloc_practice/counter/counter_observer.dart';
-import 'package:bloc_practice/infinite_list/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  Bloc.observer = const CounterObserver();
-  Bloc.observer = const SimpleBlocObserver();
+  // Bloc.observer = const CounterObserver();
+  // Bloc.observer = const SimpleBlocObserver();
   runApp(const App());
 }
