d5b7488eb2b850ed3bea5fb29b13fd1ffc36b9d3
이기종 <rlwhd0716@gmail.com>
2024-12-18 16:45:19 +0900

    [flutter firebase login] example copy

---
+++ flutter_firebase_login/lib/app/app_bloc_observer.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+  }
+}
